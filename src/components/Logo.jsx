4c662e5cd0a3ecc8e2732a39f252ad686b6b6474
Tetrabiter <noa.eskenkot@gmail.com>
2025-01-11 11:31:15 +0500

    fix mobile design

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import LogoSrc from "../assets/logo.svg"
 
 const Logo = () =>{
     return(
-        <div className="mb-[15%] sm:mb-[5%]">
+        <div className="mb-[15%] mt-[15%] sm:mt-0 sm:mb-[5%]">
             <img src={LogoSrc} alt="Ping Logo" />
         </div>
     )
